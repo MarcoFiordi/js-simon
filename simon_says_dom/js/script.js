d723b4bdbee6e4d21388b15eab8df1e43cd2256c
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-07 15:58:46 +0200

    aggiunta logica per generare 5 numeri random univoci

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -8,3 +8,16 @@ const inputElements = document.querySelectorAll('#input-group input');
 const messageElement = document.querySelector('#message');
 
 const randomNumbers = [];
+
+
+
+while (randomNumbers.length < 5){
+    const randomNumber = getRandomNumber(1, 50);
+
+    if (!randomNumbers.includes(randomNumber)){
+        randomNumbers.push(randomNumber);
+    }
+}
+
+
+console.log(randomNumbers);
