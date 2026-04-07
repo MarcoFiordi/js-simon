f30e0006a96b5b31913798de46b93d3ebb1b18cb
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-07 15:46:48 +0200

    aggiunta funzione per generare numeri random in un range

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -6,3 +6,5 @@ const numberListElement = document.querySelector('#number-list');
 const answersFormElement = document.querySelector('#answers-form');
 const inputElements = document.querySelectorAll('#input-group input');
 const messageElement = document.querySelector('#message');
+
+const randomNumbers = [];
