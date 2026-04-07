a3826789fb4741895f4d24146390efa1abde96ec
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-07 15:25:41 +0200

    aggiunti selettori DOM per elementi principali

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,1 +1,8 @@
 'use strict'
+
+const countdownElement = document.querySelector('#countdown');
+const instructionsElement = document.querySelector('#instruction');
+const numberListElement = document.querySelector('#number-list');
+const answersFormElement = document.querySelector('#answers-form');
+const inputElements = document.querySelectorAll('#input-group input');
+const messageElement = document.querySelector('#message');
